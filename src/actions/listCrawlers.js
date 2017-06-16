1671a3d77b4b44e0a0f00a2c97d4963aa3e62e5e
JakubDrobnik <drobnik.j@gmail.com>
2017-06-16 13:55:00 +0200

    bugfix: outputs more than 50 000 string length to stdout in docker container

--- src/actions/listCrawlers.js
+++ src/actions/listCrawlers.js
@@ -1,16 +1,21 @@
 /**
- * Outputs list of crawlers to console
+ * Outputs list of crawlers to stdout
  */
 export default async function listCrawlersAction(crawlerClient) {
     const crawlers = await crawlerClient.listCrawlers();
     const res = [];
     crawlers.items.forEach((crawler) => {
-        const url = `https://www.apifier.com/crawlers/${crawler._id}`;
+        const settingsLink = `https://www.apifier.com/crawlers/${crawler._id}`;
         res.push({
             id: crawler._id,
             customId: crawler.customId,
-            url,
+            settingsLink,
         });
     });
-    process.stdout.write(JSON.stringify(res));
+    const crawlersListOut = JSON.stringify(res);
+    // You can output in stdout only 64 000 bit in docker container (in plain nodejs process it works at all)
+    const maxChunkLength = 50000;
+    for (let i = 0; i < crawlersListOut.length; i += maxChunkLength) {
+        process.stdout.write(crawlersListOut.substring(i, i + maxChunkLength));
+    }
 }
