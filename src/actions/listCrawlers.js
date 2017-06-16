ecf3ec3015b433b44bc5d881f5730bdec964cba4
JakubDrobnik <drobnik.j@gmail.com>
2017-06-16 12:12:33 +0200

    smaller key because stdout file size

--- src/actions/listCrawlers.js
+++ src/actions/listCrawlers.js
@@ -5,11 +5,11 @@ export default async function listCrawlersAction(crawlerClient) {
     const crawlers = await crawlerClient.listCrawlers();
     const res = [];
     crawlers.forEach((crawler) => {
-        const settingsLink = `https://www.apifier.com/crawlers/${crawler._id}`;
+        const url = `https://www.apifier.com/crawlers/${crawler._id}`;
         res.push({
             id: crawler._id,
             customId: crawler.customId,
-            settingsLink,
+            url,
         });
     });
     process.stdout.write(JSON.stringify(res));
