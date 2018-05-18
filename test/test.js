fc58244c3bf5224fa178cee1cf6030777dc66bca
JakubDrobnik <drobnik.j@gmail.com>
2018-05-18 11:34:10 +0200

    add sample test with mocha + add test to build

---
+++ test/test.js
@@ -0,0 +1,8 @@
+const { expect } = require('chai');
+describe('Array', () => {
+    describe('#indexOf()', () => {
+        it('should return -1 when the value is not present', function() {
+            expect(true).to.equal(true);
+        });
+    });
+});
