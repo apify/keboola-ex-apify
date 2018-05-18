16919d042f16646770772f0f8b8d242819cb8135
JakubDrobnik <drobnik.j@gmail.com>
2018-05-18 11:39:01 +0200

    update test, it should stop build

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const { expect } = require('chai');
 describe('Array', () => {
     describe('#indexOf()', () => {
         it('should return -1 when the value is not present', function() {
-            expect(true).to.equal(true);
+            expect(true).to.equal(false);
         });
     });
 });
