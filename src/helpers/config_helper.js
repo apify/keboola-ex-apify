9afe75cf272c2194e0fcc53e54371fb0990febbf
drobnikj <drobnik.j@gmail.com>
2021-04-26 14:27:18 +0200

    Revert "Added debug logs"
    This reverts commit fb43c539ea76a81340297c54787471d52e8e24ff.

--- src/helpers/config_helper.js
+++ src/helpers/config_helper.js
@@ -1,6 +1,5 @@
 const nconf = require('nconf');
 const isThere = require('is-there');
-const fs = require('fs');
 
 /**
  * Reads the config and parse the input JSON object.
@@ -8,8 +7,6 @@ const fs = require('fs');
  */
 module.exports = function getConfig(configPath) {
     if (isThere(configPath)) {
-        console.log('Raw config file:');
-        console.log(fs.readFileSync(configPath).toString());
         return nconf.env().file(configPath);
     }
     console.error('No configuration specified!');
