fb43c539ea76a81340297c54787471d52e8e24ff
drobnikj <drobnik.j@gmail.com>
2021-04-26 14:13:00 +0200

    Added debug logs

--- src/helpers/config_helper.js
+++ src/helpers/config_helper.js
@@ -1,5 +1,6 @@
 const nconf = require('nconf');
 const isThere = require('is-there');
+const fs = require('fs');
 
 /**
  * Reads the config and parse the input JSON object.
@@ -7,6 +8,8 @@ const isThere = require('is-there');
  */
 module.exports = function getConfig(configPath) {
     if (isThere(configPath)) {
+        console.log('Raw config file:');
+        console.log(fs.readFileSync(configPath).toString());
         return nconf.env().file(configPath);
     }
     console.error('No configuration specified!');
