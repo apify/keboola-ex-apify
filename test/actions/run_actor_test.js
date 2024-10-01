ad85592694bae26eebbcaa118c73b5c721cde5ed
drobnikj <drobnik.j@gmail.com>
2024-10-01 16:38:31 +0200

    fix: Fix test which depends on specific Actor

--- test/actions/run_actor_test.js
+++ test/actions/run_actor_test.js
@@ -1,13 +1,63 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
 const { apifyClient, getLocalResultRows, checkRows, saveInputFile,
     actionsTestsSetup, actionsTestsTeardown, getDatasetItemsRows } = require('../src/config');
 const runActorAction = require('../../src/actions/run_actor');
+const { createAndBuildDummyActor } = require('../src/helpers');
 
-// TODO: It not a good design that tests depend on an actor, fix this.
-const TEST_ACTOR_ID = 'g4kx2tPptErWMFCra';
+chai.use(chaiAsPromised);
+
+const testingActorSourceCode = `
+        const Apify = require('apify');
+        Apify.main(async () => {
+            const { pages = 10 } = await Apify.getInput();
+            for (let i = 0; i < 100 * pages; i++) {
+                await Apify.pushData({ i, sort: Math.random(), const: 'test' });
+            }
+        });
+        `;
+
+const testingActorInputSchema = {
+    title: 'Test',
+    type: 'object',
+    schemaVersion: 1,
+    properties: {
+        pages: {
+            title: 'Pages',
+            description: 'Bla',
+            type: 'integer',
+        },
+        test: {
+            title: 'Items',
+            description: 'Bla',
+            type: 'object',
+            editor: 'json',
+        },
+        inputTableRecord: {
+            title: 'Input table',
+            description: 'Bla',
+            type: 'object',
+            editor: 'json',
+        },
+    },
+};
 
 describe('Run Actor', () => {
+    let actorId;
+    before(async () => {
+        // Create test actor
+        const actor = await createAndBuildDummyActor({
+            sourceCode: testingActorSourceCode,
+            inputSchema: testingActorInputSchema,
+        });
+        actorId = actor.id;
+    });
+    after(async () => {
+        await apifyClient.actor(actorId).delete();
+    });
+
     // Setup test
     beforeEach(actionsTestsSetup);
 
@@ -19,7 +69,7 @@ describe('Run Actor', () => {
                 hello: 'test',
             },
         };
-        await runActorAction({ apifyClient, actorId: TEST_ACTOR_ID, input: actorInput });
+        await runActorAction({ apifyClient, actorId, input: actorInput });
         const runId = console.log.args[0][0].match(/runId:\s(\w+)/)[1];
         console.log.restore();
 
@@ -38,7 +88,7 @@ describe('Run Actor', () => {
         const inputFileString = 'column,column2,column3\ntest,value,1\ntest2,value2,2\n';
         await saveInputFile(inputFileString);
         sinon.spy(console, 'log');
-        await runActorAction({ apifyClient, actorId: TEST_ACTOR_ID, input: {} });
+        await runActorAction({ apifyClient, actorId, input: {} });
         const runId = console.log.args[0][0].match(/runId:\s(\w+)/)[1];
         console.log.restore();
 
@@ -60,7 +110,7 @@ describe('Run Actor', () => {
         };
         const fields = ['i', 'sort'];
         sinon.spy(console, 'log');
-        await runActorAction({ apifyClient, actorId: TEST_ACTOR_ID, input: actorInput, fields: fields.join(',') });
+        await runActorAction({ apifyClient, actorId, input: actorInput, fields: fields.join(',') });
         const runId = console.log.args[0][0].match(/runId:\s(\w+)/)[1];
         console.log.restore();
 
